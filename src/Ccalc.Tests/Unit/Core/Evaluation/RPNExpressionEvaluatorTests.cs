2f8506bb0c95deb96949e5b85f3191609f387b22
CIRCULARKA <ruslan.getmail@gmail.com>
2024-11-12 21:40:25 +0500

    feat: Append extra test cases

--- src/Ccalc.Tests/Unit/Core/Evaluation/RPNExpressionEvaluatorTests.cs
+++ src/Ccalc.Tests/Unit/Core/Evaluation/RPNExpressionEvaluatorTests.cs
@@ -11,14 +11,14 @@ public class RPNExpressionEvaluatorTests
 
     [Theory]
     // Generated with ChatGPT, THANKS. Verified
-    [InlineData("5 3 +", 8)]
-    [InlineData("6 2 -", 4)]
+    [InlineData("5 3 +   ", 8)]
+    [InlineData("6 2   -", 4)]
     [InlineData("7 3 *", 21)]
-    [InlineData("12 4 /", 3)]
+    [InlineData("12   4 /", 3)]
     [InlineData("4 2 3 * +", 10)]
     [InlineData("8 3 2 + *", 40)]
     [InlineData("10 5 2 - /", 3.3333333)]
-    [InlineData("9 2 + 3 *", 33)]
+    [InlineData("9 2 +   3 *", 33)]
     [InlineData("15 7 1 1 + - /", 3)]
     public void Evaluate_ValidRPNExpression_ExpectedResult(string rpnExpression, double expectedResult)
     {
@@ -29,8 +29,9 @@ public class RPNExpressionEvaluatorTests
         var result = evaluator.Evaluate(rpnExpression);
         
         // Assert
-        Assert.True(result.IsSuccessful);
+        Assert.NotNull(result);
         Assert.NotNull(result.Result);
+        Assert.True(result.IsSuccessful);
         Assert.Null(result.ErrorMessage);
 
         Assert.Equal(
@@ -82,6 +83,7 @@ public class RPNExpressionEvaluatorTests
     {
         ArgumentNullException.ThrowIfNull(result);
 
+        Assert.NotNull(result);
         Assert.Null(result.Result);
         Assert.False(result.IsSuccessful);
         Assert.True(string.IsNullOrWhiteSpace(result.ErrorMessage) is false);
