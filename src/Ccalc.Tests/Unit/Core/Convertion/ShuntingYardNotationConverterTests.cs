ff258bcb19fffac3d5b3825d65989c0b0d1e383a
CIRCULARKA <ruslan.getmail@gmail.com>
2024-11-12 21:47:15 +0500

    tests: Add border and failure cases

--- src/Ccalc.Tests/Unit/Core/Convertion/ShuntingYardNotationConverterTests.cs
+++ src/Ccalc.Tests/Unit/Core/Convertion/ShuntingYardNotationConverterTests.cs
@@ -1,21 +1,79 @@
 namespace Ccalc.Tests.Infrastructure;
 
+[Trait("type", "unit")]
+[Trait("category", "yard")]
 public class ShuntingYardNotationConverterTests
 {
     [Theory]
     [InlineData("1 + 2", "1 2 +")]
-    [InlineData("1 - 2 / 3", "1 2 3 / -")]
-    [InlineData("(1 - 1) * (2 + 1) / 3", "1 1 2 1 + - * 3 /")]
-    public void ToPostfix_ValidInfixExpression_ValidPostfixExpression(string infixExpression, string expected)
+    [InlineData("1 - 2 / 3.001", "1 2 3.001 / -       ")]
+    [InlineData("   (1 - 1) * (2.33 + 1) / 3", "1 1 2.33    1 + - * 3 /")]
+    public void ToPostfix_ValidInfixExpression_ValidPostfixExpression(string infixExpression, string expectedResult)
     {
         // Arrange
         var converter = CreateConverter();
 
         // Act
-        var actual = converter.ToPostfix(infixExpression);
+        var result = converter.ToPostfix(infixExpression);
 
         // Assert
-        Assert.Equal(expected, actual);
+        Assert.NotNull(result);
+        Assert.True(result.IsSuccessful);
+        Assert.True(string.IsNullOrWhiteSpace(result.ErrorMessage));
+        Assert.Equal(expectedResult, result.Result);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData("()")]
+    [InlineData("1 2 +")]
+    [InlineData("1 + 2 + 3 * 4 / 1 -")]
+    [InlineData("+")]
+    [InlineData("2 1")]
+    [InlineData("5.015 + 1 - (1 + 1) * 2 2")]
+    [InlineData("15.01 / 3 * (2 + 2")]
+    [InlineData("15.01 / 3 * 2 + 2)")]
+    [InlineData("15.01 / 3 * (2 + 2))")]
+    public void ToPostfix_InvalidInfixExpression_ErrorResult(string infixExpession)
+    {
+        // Arrange
+        var converter = CreateConverter();
+
+        // Act
+        var result = converter.ToPostfix(infixExpession);
+
+        // Assert
+        Assert.NotNull(result);
+        CheckIfError(result);
+    }
+
+    [Theory]
+    [InlineData("1 ^ 2")]
+    [InlineData("1 - 2 _ 3.001")]
+    [InlineData("(1 - 1) * (2.33 $ 1) / 3")]
+    public void ToPostfix_ValidInfixExpressionWithUnsupportedOperators_ErrorResult(string infixExpession)
+    {
+        // Arrange
+        var converter = CreateConverter();
+
+        // Act
+        var result = converter.ToPostfix(infixExpession);
+
+        // Assert
+        Assert.NotNull(result);
+        CheckIfError(result);
+    }
+
+    /// <summary>
+    /// Проверяет, является ли результат преобразования ошибочным
+    /// </summary>
+    /// <param name="result">Результат, который нужно проверить</param>
+    private void CheckIfError(ConvertionResult result)
+    {
+        Assert.Null(result.Result);
+        Assert.False(result.IsSuccessful);
+        Assert.False(string.IsNullOrWhiteSpace(result.ErrorMessage));
     }
 
     private ShuntingYardNotationConverter CreateConverter()
