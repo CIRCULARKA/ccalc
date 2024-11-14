438d95e651921fc1ab3d973b900a4b1a53eae750
CIRCULARKA <ruslan.getmail@gmail.com>
2024-11-14 12:50:43 +0500

    feat: Add definition of Calculator class

---
+++ src/Ccalc.Core/Calculator.cs
@@ -0,0 +1,25 @@
+namespace Ccalc.Core;
+
+/// <summary>
+/// Класс, вычисляющий математические выражения
+/// </summary>
+public class Calculator
+{
+    private readonly INotationConverter _notationConverter;
+
+    private readonly IExpressionEvaluator _evaluator;
+
+    public Calculator(INotationConverter notationConverter, IExpressionEvaluator evaluator)
+    {
+        ArgumentNullException.ThrowIfNull(notationConverter);
+        ArgumentNullException.ThrowIfNull(evaluator);
+
+        _notationConverter = notationConverter;
+        _evaluator = evaluator;
+    }
+
+    public double Evaluate(string mathExpression)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Ccalc.Core/Usings.cs
+++ src/Ccalc.Core/Usings.cs
@@ -1,3 +1,4 @@
 global using Ccalc.Core.Common;
 global using Ccalc.Core.Parsing;
 global using Ccalc.Core.Evaluation;
+global using Ccalc.Core.Convertion;
