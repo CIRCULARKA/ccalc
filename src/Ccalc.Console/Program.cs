8fcc77710b91fa35018392abc989fee13b6473ef
CIRCULARKA <ruslan.getmail@gmail.com>
2024-11-14 14:05:47 +0500

    feat: Implement console application that utilizes calculator

--- src/Ccalc.Console/Program.cs
+++ src/Ccalc.Console/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Any() is false)
+    Console.WriteLine("Usage: ccalc <expression>");
+
+var supportedOperations = new List<Operator>
+{
+    new Operator(
+        "+",
+        1,
+        OperatorAssociativity.Left,
+        (a, b) => a + b),
+    new Operator(
+        "-",
+        1,
+        OperatorAssociativity.Left,
+        (a, b) => a - b),
+    new Operator(
+        "*",
+        2,
+        OperatorAssociativity.Left,
+        (a, b) => a * b),
+    new Operator(
+        "/",
+        2,
+        OperatorAssociativity.Left,
+        (a, b) => a / b),
+};
+
+var expression = string.Join(" ", args);
+
+var calculator = Calculator.CreateDefaultCalculator(supportedOperations);
+
+var result = calculator.Evaluate(expression);
+
+if (result.IsSuccessful)
+    Console.WriteLine(result.Result);
+else
+    Console.WriteLine(result.ErrorMessage);
---
+++ src/Ccalc.Console/Usings.cs
@@ -0,0 +1,5 @@
+global using Ccalc.Core;
+global using Ccalc.Core.Common;
+global using Ccalc.Core.Parsing;
+global using Ccalc.Core.Convertion;
+global using Ccalc.Core.Evaluation;
